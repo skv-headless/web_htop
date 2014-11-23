e97c8ab4bc01977b00e4389c5006b20537ba3c55
skv-headless <skv-headless@yandex.ru>
2014-11-23 17:55:09 +0300

    proc api

--- app.py
+++ app.py
@@ -9,8 +9,20 @@ def index():
 
 @app.route("/stats")
 def stats():
+    processes = []
+    for proc in psutil.process_iter():
+        try:
+            pinfo = proc.as_dict(attrs=['pid', 'name'])
+            pinfo['cpu_percent'] = proc.cpu_percent(interval=None)
+            pinfo['memory_percent'] = proc.memory_percent()
+        except psutil.NoSuchProcess:
+            pass
+        else:
+            processes.append(pinfo)
+
     return json.dumps({
-        'cpu_load': psutil.cpu_percent(interval=1, percpu=True)
+        'cpu_load': psutil.cpu_percent(interval=None, percpu=True),
+        'processes': processes,
         })
 
 if __name__ == "__main__":
